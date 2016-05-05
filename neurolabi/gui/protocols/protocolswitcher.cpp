324192b2ccc691694f8d629c33a3bc22f0c94ff0
Donald J. Olbris <olbrisd@janelia.hhmi.org>
2016-05-05 09:17:38 -0400

    continued work; laid out logic in comments

--- neurolabi/gui/protocols/protocolswitcher.cpp
+++ neurolabi/gui/protocols/protocolswitcher.cpp
@@ -16,20 +16,38 @@
 ProtocolSwitcher::ProtocolSwitcher(QWidget *parent) : QObject(parent)
 {
     m_parent = parent;
+    m_chooser = new ProtocolChooser(m_parent);
+
+
+    // set up connections to ProtocolChooser
+
+
+
 }
 
 void ProtocolSwitcher::openProtocolRequested() {
 
     std::cout << "entering prsw::openProtocolRequested()"<< std::endl;
 
-    // main logic:
+
+    // testing
     if (m_currentDvidTarget.isValid()) {
-        // testing
-        m_chooser = new ProtocolChooser(m_parent);
         m_chooser->show();
         m_chooser->raise();
-
     }
+
+
+
+    // actual logic
+    // if protocol is active and loading:
+    //  if protocol is done loading:
+    //      show the protocol dialog
+    //  else
+    //      (how to wait for protocol dialog to be ready to show?)
+    //      (does protocol dialog need to work like chooser, immediately show 
+    //      while loading, then populate?)
+    // else
+    //  show the protocol chooser
 
 
 
@@ -41,8 +59,26 @@ void ProtocolSwitcher::dvidTargetChanged(ZDvidTarget target) {
 
 
     // probably should check for active protocol here and start loading
-    //  even before user decides to open the window?
+    //  even before user decides to open the window?  
 
+    // also check for paused protocols that could be loaded?
+    // trigger population of ProtocolChooser here?
+
+
+    // retrieve protocol metadata
+    //  (need new ProtocolMetadata class)
+    // if there's an active protocol:
+    //  set "protocol loading" flag
+    //  set "active protocol" flag
+    //  load it: create, populate dialog; 
+    // if no:
+    //  unset "active protocol" flag
+    //  populate chooser:
+    //      "loading" message
+    //      check for inactive, incomplete protocols we could load; 
+    //          add to loadable list
+    //      add allowable new protocols to new protocol list
+    //      clear "loading" message
 
 }
 
