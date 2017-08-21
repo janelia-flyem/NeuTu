429fe1d7a0a5040eb393b7139b9ca4ef4fafefa5
Ting Zhao <tingzhao@gmail.com>
2017-08-21 08:55:01 -0400

    add split service

---
+++ neurolabi/python/service/split_server.py
@@ -0,0 +1,44 @@
+import timer
+import threading
+import time
+import argparse
+import yaml
+
+from pyzem.dvid import dvidenv
+from pyzem.dvid import dvidio
+from pyzem.compute import bodysplit
+
+parser = argparse.ArgumentParser(description='Process arguments for running splitting service')
+parser.add_argument('--config', dest='config', type=str, help='Configuration file in YAML format')
+parser.add_argument('--info', help='Show task information', action='store_true')
+args=parser.parse_args()
+print args.config
+
+with open(args.config, 'r') as fp:
+    config = yaml.load(fp)
+
+taskServer = config['task_server']
+
+taskEnv = dvidenv.DvidEnv(host = taskServer['host'], port=taskServer['port'], uuid=taskServer['uuid'])
+split = bodysplit.BodySplit(config['command'], taskEnv)
+print split._neutu
+#split.run('task__http-++emdata1.int.janelia.org-8500+api+node+b6bc+bodies+sparsevol+12007338')
+
+dc = dvidio.DvidClient(env = taskEnv)
+if args.info:
+    dc.print_split_task()
+    exit()
+
+splitTaskList = dc.read_split_task_keys()
+
+def process():
+    while True:
+        splitTaskList = dc.read_split_task_keys()
+        print splitTaskList
+        for task in splitTaskList:
+            split.run(task)
+        time.sleep(10)
+
+process()
+#threading.Timer(1, process).start()
+
