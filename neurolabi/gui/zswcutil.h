3d10781a3d42c814fc4b3fc9c3a1d79c9c4a185d
Ting Zhao <tingzhao@gmail.com>
2017-11-09 11:16:58 -0500

    add macro control for variadics

--- neurolabi/gui/zswcutil.h
+++ neurolabi/gui/zswcutil.h
@@ -2,6 +2,7 @@
 #define ZSWCUTIL_H
 
 #include <vector>
+#include <set>
 #include "zswctree.h"
 
 namespace ZSwc
@@ -18,15 +19,33 @@ std::vector<Swc_Tree_Node*> FindOverlapNode(
 
 void Subtract(ZSwcTree *tree1, const ZSwcTree *tree2);
 
+#if __cplusplus == 201103L
 template <template<class...> class container>
 void SetType(container<Swc_Tree_Node*> nodeGroup, int type);
+#define _SWC_SET_TYPE_DEFINED 1
+#else
+void SetType(const std::set<Swc_Tree_Node*> &nodeSet, int type);
+#endif
+
+template <typename InputIterator>
+void SetType(const InputIterator &first, const InputIterator &last, int type);
 }
 
+#if __cplusplus == 201103L
 template <template<class...> class container>
 void ZSwc::SetType(container<Swc_Tree_Node *> nodeGroup, int type)
 {
   for (typename container<Swc_Tree_Node*>::iterator iter = nodeGroup.begin();
        iter != nodeGroup.end(); ++iter) {
+    SwcTreeNode::setType(*iter, type);
+  }
+}
+#endif
+
+template <typename InputIterator>
+void ZSwc::SetType(const InputIterator &first, const InputIterator &last, int type)
+{
+  for (InputIterator iter = first; iter != last; ++iter) {
     SwcTreeNode::setType(*iter, type);
   }
 }
