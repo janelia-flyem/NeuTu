0032d63b0ffeec7e4a3dc2bf6650b9c310633e82
Donald J. Olbris <olbrisd@janelia.hhmi.org>
2017-11-14 11:12:44 -0500

    branch panel starting to work; populated and clickable

--- neurolabi/gui/dialogs/dvidbranchdialog.cpp
+++ neurolabi/gui/dialogs/dvidbranchdialog.cpp
@@ -14,6 +14,7 @@
 #include <QStringListModel>
 #include <QsLog.h>
 
+#include "dvid/zdvidtarget.h"
 
 DvidBranchDialog::DvidBranchDialog(QWidget *parent) :
     QDialog(parent),
@@ -21,9 +22,11 @@ DvidBranchDialog::DvidBranchDialog(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    m_reader.setVerbose(false);
 
     // UI connections
     connect(ui->repoListView, SIGNAL(clicked(QModelIndex)), this, SLOT(onRepoClicked(QModelIndex)));
+    connect(ui->branchListView, SIGNAL(clicked(QModelIndex)), this, SLOT(onBranchClicked(QModelIndex)));
 
 
     // data load connections
@@ -60,6 +63,10 @@ const QString DvidBranchDialog::KEY_SERVER = "server";
 const QString DvidBranchDialog::KEY_PORT = "port";
 const QString DvidBranchDialog::KEY_UUID = "UUID";
 const QString DvidBranchDialog::KEY_DESCRIPTION = "description";
+const QString DvidBranchDialog::KEY_DAG = "DAG";
+const QString DvidBranchDialog::KEY_NODES = "Nodes";
+
+const QString DvidBranchDialog::MESSAGE_LOADING = "Loading...";
 
 /*
  * load stored dataset list into the first panel of the UI
@@ -110,31 +117,127 @@ QJsonObject DvidBranchDialog::loadDatasetsFromFile() {
         showError("Error parsing file", "Couldn't parse file " + filepath + "!");
         return empty;
     } else {
-        LINFO() << "Task protocol: json loaded from file" + filepath;
+        LINFO() << "Read DVID repo information json from file" + filepath;
         return doc.object();
     }
 }
 
 void DvidBranchDialog::onRepoClicked(QModelIndex modelIndex) {
-
-    QString itemString = m_repoModel->data(modelIndex, Qt::DisplayRole).toString();
-
-    std::cout << "in onRepoClicked(); got " << itemString.toStdString() << std::endl;
-
-
-    // clear existing view and model?
-    // loading message?
-
-
-    // do the read
-
-    // store stuff in intermediate form (presumably)
-
-    // populate the branch model
-
-
+    loadBranches(m_repoModel->data(modelIndex, Qt::DisplayRole).toString());
 }
 
+/*
+ * given a repo name, load all its branches into the UI
+ */
+void DvidBranchDialog::loadBranches(QString repoName) {
+    LINFO() << "Loading branches from DVID repo" << repoName.toStdString();
+
+    // clear model; use it as a loading message, too
+    QStringList branchNames;
+    branchNames.append(MESSAGE_LOADING);
+    m_branchModel->setStringList(branchNames);
+
+
+    // read repo info, in this thread; there's nothing else the user can do
+    //  but wait at this point anyway
+    ZDvidTarget target(m_repoMap[repoName][KEY_SERVER].toString().toStdString(),
+        m_repoMap[repoName][KEY_UUID].toString().toStdString(),
+        m_repoMap[repoName][KEY_PORT].toInt());
+    if (!m_reader.open(target)) {
+        QMessageBox errorBox;
+        errorBox.setText("Error connecting to DVID");
+        errorBox.setInformativeText("Could not open DVID!  Check server information!");
+        errorBox.setStandardButtons(QMessageBox::Ok);
+        errorBox.setIcon(QMessageBox::Warning);
+        errorBox.exec();
+        branchNames.clear();
+        m_branchModel->setStringList(branchNames);
+        return;
+    }
+    ZJsonObject info = m_reader.readInfo();
+    QJsonDocument doc = QJsonDocument::fromJson(QString::fromStdString(info.dumpString()).toUtf8());
+    QJsonObject repoJson = doc.object();
+
+    // parse out actual branches and populate the model
+    QJsonObject nodeJson = repoJson[KEY_DAG].toObject()[KEY_NODES].toObject();
+    QStringList branchUUIDs = findBranches(nodeJson);
+
+    branchNames.clear();
+    foreach (QString uuid, branchUUIDs) {
+        QJsonObject branchNode = nodeJson[uuid].toObject();
+        QString branchName = branchNode["Branch"].toString();
+        m_branchMap[branchName] = branchNode;
+        branchNames.append(branchName);
+    }
+
+    m_branchModel->setStringList(branchNames);
+}
+
+/*
+ * given the DVID-returned node json for a repo, return a list
+ * of the UUID keys into that json that correspond to the nodes
+ * that are tips of branches
+ */
+QStringList DvidBranchDialog::findBranches(QJsonObject nodeJson) {
+
+    QStringList UUIDs;
+
+    // branches should have nodes in a parent-child relationship;
+    //  the node with a given branch name that has no children with
+    //  the branch name is the tip (since it can have children with
+    //  different branch names)
+
+    // problem is that we have to be able to handle old repos that don't
+    //  enforce the branch convention; eg, tons of unnamed branches that
+    //  have no relation to each other
+
+    // first pass: separate by branch name; accumulate parent versions
+
+    // branch name: list of uuids
+    QMap<QString, QStringList> branches;
+    // branch name: set of parent version IDs
+    QMap<QString, QSet<int>> parents;
+
+    foreach (QString uuid, nodeJson.keys()) {
+        QJsonObject node = nodeJson[uuid].toObject();
+        QString branchName = node["Branch"].toString();
+        branches[branchName].append(uuid);
+        foreach (QJsonValue parent, node["Parents"].toArray()) {
+            parents[branchName].insert(parent.toInt());
+        }
+    }
+
+    // second pass: for each branch, find the tip; if a node isn't the
+    //  parent of any other node in the branch, it's a tip; if there's more than
+    //  one, that's a result of older repos that don't respect the rules
+    //  we have no way to pick a commit, so eliminate it from the list (for now)
+    foreach (QString branchName, branches.keys()) {
+        QList<QString> tiplist;
+        foreach (QString uuid, branches[branchName]) {
+            QJsonObject node = nodeJson[uuid].toObject();
+            if (!parents[branchName].contains(node["VersionID"].toInt())) {
+                tiplist.append(uuid);
+            }
+        }
+        if (tiplist.size() != 1) {
+            LINFO() << "Branch" << branchName.toStdString() << "has indeterminate tip";
+        } else {
+            UUIDs.append(tiplist[0]);
+        }
+    }
+    return UUIDs;
+}
+
+void DvidBranchDialog::onBranchClicked(QModelIndex modelIndex) {
+
+    QString branchName = m_branchModel->data(modelIndex, Qt::DisplayRole).toString();
+
+    // this is kind of ad hoc:
+    if (branchName != MESSAGE_LOADING) {
+        LINFO() << "branch clicked:" << branchName;
+    }
+
+}
 
 /*
  * input: title and message for error dialog
