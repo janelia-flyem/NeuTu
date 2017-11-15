d51cad360ab220dba62d569ab19e025ef4bfb30e
deli <ephemera_2015@sina.com>
2017-11-15 12:10:03 +0800

    fix bugs

--- neurolabi/gui/imgproc/zstackmultiscalewatershed.h
+++ neurolabi/gui/imgproc/zstackmultiscalewatershed.h
@@ -12,7 +12,7 @@ class ZStack;
 class ZSwcTree;
 class ZIntPoint;
 class ZObject3d;
-
+class ZIntCuboid;
 class ZStackMultiScaleWatershed
 {
 public:
@@ -26,14 +26,17 @@ public:
 
 public:
   void setScale(int scale){m_scale=scale;}
-  ZStack* labelAreaNeedUpdate(ZStack* edge_map,ZStack* seed,ZStack* src=NULL);
-  void generateSeeds(ZStack* seed,int width,int height,int depth,const ZStack* edge_map,const ZStack* stack);
-  ZStack* getEdgeMap(const ZStack& stack);
+  ZStack* labelAreaNeedUpdate(ZStack* boundary_map,ZStack* seed,ZIntCuboid& boundbox,ZStack* src=NULL);
+  void generateSeeds(ZStack* seed,int width,int height,int depth,const ZStack* boundary_map,const ZStack* stack);
+  ZStack* getBoundaryMap(const ZStack& stack);
   ZStack* upSample(int width,int height,int depth,ZStack* sampled);
-  ZStack* upSampleAndRecoverEdge(ZStack* sampled_watershed,ZStack* src);
+  ZStack* upSampleAndRecoverBoundary(ZStack* sampled_watershed,ZStack* src);
 
 private:
   ZStack* toSeedStack(std::vector<ZObject3d*>& seeds,int width,int height,int depth,ZIntPoint offset);
+  inline void addSeedX(ZStack* pSeed,int x,int sy,int ey,int sz,int ez,uint8_t v);
+  inline void addSeedY(ZStack* pSeed,int y,int sx,int ex,int sz,int ez,uint8_t v);
+  inline void addSeedZ(ZStack* pSeed,int z,int sx,int ex,int sy,int ez,uint8_t v);
 private:
   int m_scale;
 };
