4b8f9f732857bf20c55a8de9b31eea4362ac2818
Ting Zhao <tingzhao@gmail.com>
2017-11-30 13:27:56 -0500

    add conda build script

---
+++ conda_build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+if [ $# == 0 ]
+then
+  echo "Usage: sh conda_build.sh <neutu/neu3/neutu-develop/neu3-develop/neutu-debug/neu3-debug>"
+  exit 1
+fi
+
+export NEUTU_TARGET=$1
+
+recipe=recipe-neutu
+conda build $recipe
+ 
