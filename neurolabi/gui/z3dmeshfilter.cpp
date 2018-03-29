a458116ea0fe1c7f259a295598ef9b88c796c745
Philip M. Hubbard <hubbardp@janelia.hhmi.org>
2018-03-29 10:19:08 -0400

    Clears the bounding-box cache when the mesh list changes.

--- neurolabi/gui/z3dmeshfilter.cpp
+++ neurolabi/gui/z3dmeshfilter.cpp
@@ -131,6 +131,7 @@ void Z3DMeshFilter::setData(const std::vector<ZMesh*>& meshList)
 void Z3DMeshFilter::setData(const QList<ZMesh*>& meshList)
 {
   m_origMeshList.clear();
+  m_meshBoundboxMapper.clear();
   for (auto mesh : meshList)
     m_origMeshList.push_back(mesh);
   //LOG(INFO) << className() << " read " << m_origMeshList.size() << " meshes.";
