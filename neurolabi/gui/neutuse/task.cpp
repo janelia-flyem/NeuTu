2d765a290640f38475b800e8574bc80534c51ea3
Ting Zhao <tingzhao@gmail.com>
2018-08-30 11:47:24 -0400

    deactivate cleaving shortcut when split is on

---
+++ neurolabi/gui/neutuse/task.cpp
@@ -0,0 +1,52 @@
+#include "task.h"
+
+namespace neutuse {
+
+const std::string Task::KEY_TYPE = "type";
+const std::string Task::KEY_NAME = "name";
+const std::string Task::KEY_DESCRIPTION = "description";
+const std::string Task::KEY_PRIORITY = "priority";
+const std::string Task::KEY_LIFE_SPAN = "life_span";
+const std::string Task::KEY_MAX_TRIES = "max_tries";
+const std::string Task::KEY_CONFIG = "config";
+const std::string Task::KEY_USER = "user";
+
+Task::Task()
+{
+}
+
+ZJsonObject Task::toJsonObject() const
+{
+  ZJsonObject obj;
+  obj.setEntry(KEY_TYPE, m_type);
+  obj.setEntry(KEY_NAME, m_name);
+  obj.setEntry(KEY_PRIORITY, m_priority);
+  obj.setEntry(KEY_LIFE_SPAN, m_lifeSpan);
+  obj.setEntry(KEY_MAX_TRIES, m_maxTries);
+  obj.setEntry(KEY_USER, m_user);
+  obj.setEntry(KEY_CONFIG.c_str(), m_config.getData());
+
+  return obj;
+}
+
+void Task::setType(const std::string &type)
+{
+  m_type = type;
+}
+
+void Task::setName(const std::string &name)
+{
+  m_name = name;
+}
+
+void Task::setUser(const std::string &user)
+{
+  m_user = user;
+}
+
+void Task::setConfig(const ZJsonObject &config)
+{
+  m_config = config;
+}
+
+}
