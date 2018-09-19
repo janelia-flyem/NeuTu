a42da5e860ef44dd6ab91509c4af600b17cc6a5b
Philip M. Hubbard <hubbardp@janelia.hhmi.org>
2018-09-19 10:52:10 -0400

    Updates logging to correctly identify 'tarsupervoxels'.

--- neurolabi/gui/protocols/taskbodycleave.cpp
+++ neurolabi/gui/protocols/taskbodycleave.cpp
@@ -1245,10 +1245,17 @@ void TaskBodyCleave::cleave()
   requestJson["port"] = m_bodyDoc->getDvidTarget().getPort();
   requestJson["uuid"] = m_bodyDoc->getDvidTarget().getUuid().c_str();
   requestJson["segmentation-instance"] = m_bodyDoc->getDvidTarget().getBodyLabelName().c_str();
-  requestJson["mesh-instance"] =
-      ZDvidData::GetName(ZDvidData::ROLE_MESHES_TARS,
-                         ZDvidData::ROLE_BODY_LABEL,
-                         m_bodyDoc->getDvidTarget().getBodyLabelName()).c_str();
+  if (m_bodyDoc->usingOldMeshesTars()) {
+    requestJson["mesh-instance"] =
+        ZDvidData::GetName(ZDvidData::ROLE_MESHES_TARS,
+                           ZDvidData::ROLE_BODY_LABEL,
+                           m_bodyDoc->getDvidTarget().getBodyLabelName()).c_str();
+  } else {
+    requestJson["mesh-instance"] =
+        ZDvidData::GetName(ZDvidData::ROLE_TAR_SUPERVOXELS,
+                           ZDvidData::ROLE_BODY_LABEL,
+                           m_bodyDoc->getDvidTarget().getBodyLabelName()).c_str();
+  }
 
   // TODO: Teporary cleaving sevrver URL.
   QString server = "http://emdata3.int.janelia.org:5551/compute-cleave";
