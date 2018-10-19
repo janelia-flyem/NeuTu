ab6de7d6e6fa85bced017f38488d4c878861e536
Deli Wang <ephemera_2015@sina.com>
2018-10-19 10:43:57 +0800

    initial commit of zzegmentationscan

--- neurolabi/gui/sandbox/zmultiscalesegmentationmanagement.cpp
+++ neurolabi/gui/sandbox/zmultiscalesegmentationmanagement.cpp
@@ -24,6 +24,309 @@
 #include "mainwindow.h"
 
 
+//implementation for ZSegmentationScan
+void ZSegmentationScan::clearData()
+{
+  if(m_data.size() !=0 )
+  {
+    m_data.clear();
+  }
+  m_sz = MAX_INT32;
+  m_ez = 0;
+  m_sy = MAX_INT32;
+  m_ey = 0;
+}
+
+
+void ZSegmentationScan::unify(ZSegmentationScan *obj)
+{
+
+  int sz = std::min(minZ(), obj->minZ());
+  int ez = std::max(maxZ(), obj->maxZ());
+  int size_z = ez - sz + 1;
+
+  int sy = std::min(minY(), obj->minY());
+  int ey = std::max(maxY(), obj->maxY());
+  int size_y = ey - sy + 1;
+
+  std::vector<std::vector<std::vector<int>>> new_data;
+  new_data.resize(size_z);
+  for(int z = 0; z < size_z; ++z)
+  {
+    new_data[z].resize(size_y);
+  }
+
+  for(int z = 0; z < size_z; ++z)
+  {
+    for(int y = 0; y < size_y; ++y)
+    {
+      new_data[z][y] = canonize(getStrip(z+sz,y+sy),obj->getStrip(z+sz,y+sy));
+    }
+  }
+
+  m_data = new_data;
+  m_sz = sz;
+  m_ez = ez;
+  m_sy = sy;
+  m_ey = ey;
+}
+
+
+std::vector<int> ZSegmentationScan::canonize(std::vector<int> &a, std::vector<int> &b)
+{
+  if(a.size() == 0)
+  {
+    return b;
+  }
+  if(b.size() == 0)
+  {
+    return a;
+  }
+
+  std::vector<int> rv;
+  std::vector<int>::iterator itas = a.begin();
+  std::vector<int>::iterator itbs = b.begin();
+  std::vector<int>::iterator itae = itas + 1;
+  std::vector<int>::iterator itbe = itbs + 1;
+
+  while(itae < a.end() && itbe < b.end())
+  {
+    int a = *itas;
+    int b = *itae;
+    if(*itas < *itbs)
+    {
+      itas += 2;
+      itae += 2;
+    }
+    else
+    {
+      a = *itbs;
+      b = *itbe;
+      itbs += 2;
+      itbe += 2;
+    }
+    if(rv.size() == 0)
+    {
+      rv.push_back(a);
+      rv.push_back(b);
+    }
+    else
+    {
+      int d = rv.back();
+      rv.pop_back();
+      if(a <= d)
+      {
+        rv.push_back(b);
+      }
+      else
+      {
+        rv.push_back(d);
+        rv.push_back(a);
+        rv.push_back(b);
+      }
+    }
+  }
+
+  while(itae < a.end())
+  {
+    rv.push_back(*itas);
+    rv.push_back(*itae);
+    itas += 2;
+    itae += 2;
+  }
+  while(itbe < b.end())
+  {
+    rv.push_back(*itbs);
+    rv.push_back(*itbe);
+    itbs += 2;
+    itbe += 2;
+  }
+  return rv;
+}
+
+
+void ZSegmentationScan::preprareData(int depth, int height)
+{
+  clearData();
+  m_data.resize(depth);
+
+  for(int i = 0; i < depth; ++i)
+  {
+    m_data[i].resize(height);
+  }
+}
+
+
+ZIntCuboid ZSegmentationScan::getStackForegroundBoundBox(ZStack *stack)
+{
+  ZIntCuboid box(0,0,0,-1,-1,-1);
+  if(!stack)
+  {
+    return box;
+  }
+
+  uint8_t *p = stack->array8();
+  int width = stack->width();
+  int height = stack->height();
+  int depth = stack->depth();
+  size_t area = width * height;
+
+  int min_x = width, max_x = -1;
+  int min_y = height, max_y = -1;
+  int min_z = depth, max_z = -1;
+
+  for(int z = 0 ; z < depth; ++z)
+  {
+    for(int y = 0; y < height; ++y)
+    {
+      uint8_t *q = p + z*area + y*width;
+      for(int x = 0; x < width; ++x, ++q)
+      {
+        if(*q)
+        {
+          if(z < min_z) min_z = z;
+          if(z > max_z) max_z = z;
+          if(y < min_y) min_y = y;
+          if(y > max_y) max_y = y;
+          if(x < min_x) min_x = x;
+          if(x > max_x) max_x = x;
+        }
+      }
+    }
+  }
+  box.setFirstCorner(min_x,min_y,min_z);
+  box.setLastCorner(max_x,max_y,max_z);
+  return box;
+}
+
+
+void ZSegmentationScan::loadStack(ZStack* stack)
+{
+  if(!stack)
+  {
+    return ;
+  }
+  clearData();
+  int width = stack->width();
+  int height = stack->height();
+  //int depth = stack->depth();
+  size_t area  = width*height;
+
+  ZIntCuboid box = getStackForegroundBoundBox(stack);
+  if(box.getDepth() <= 0 || box.getHeight() <=0 || box.getWidth() <=0)
+  {
+    return ;
+  }
+
+  m_sz = box.getFirstCorner().m_z;
+  m_sy = box.getFirstCorner().m_y;
+  m_ez = box.getLastCorner().m_z;
+  m_ey = box.getLastCorner().m_y;
+
+  preprareData(box.getDepth(), box.getHeight());
+
+  uint8_t* p = stack->array8();
+  for(int k = m_sz; k <= m_ez; ++k)
+  {
+    for(int j = m_sy; j <= m_ey; ++j)
+    {
+      uint8_t * q = p + k*area + j*width + box.getFirstCorner().m_x;
+      int sx = -1, ex = -1;
+      int flag = 0;
+
+      for(int i = box.getFirstCorner().m_x; i <= box.getLastCorner().m_x; ++i, ++q)
+      {
+        if(*q && flag == 0)
+        {
+          sx = i;
+          flag = 1;
+        }
+        else if(!*q && flag ==1)
+        {
+          ex = i -1;
+          flag = 0;
+          addSegment(k,j,sx,ex);
+        }
+      }
+      if(flag ==1)
+      {
+        addSegment(k,j,sx,box.getLastCorner().m_x);
+      }
+    }
+  }
+  //setOffset(stack->getOffset());
+}
+
+
+void ZSegmentationScan::labelStack(ZStack *stack, int v)
+{
+  if(!stack)
+  {
+    return ;
+  }
+
+  uint8_t *p = stack->array8();
+  int width = stack->width();
+  int height = stack->height();
+  //int depth = stack->depth();
+  int area = width * height;
+
+  for(uint z = 0; z < m_data.size(); ++z)
+  {
+    std::vector<std::vector<int>>& dy = m_data[z];
+    for(uint y = 0; y < dy[y].size(); ++y)
+    {
+      std::vector<int>& dx = dy[y];
+      uint8_t *q = p + (z + m_sz)*area + (y + m_sy)*width;
+      for(uint x = 0; x < dx.size(); x += 2)
+      {
+        int sx = dx[x];
+        int ex = dx[x+1];
+
+        for(int i = sx; i <= ex; ++i)
+        {
+          q[i] = v;
+        }
+      }
+    }
+  }
+}
+
+
+void ZSegmentationScan::maskStack(ZStack *stack)
+{
+  if(!stack)
+  {
+    return ;
+  }
+
+  int width = stack->width();
+  int height = stack->height();
+  int depth = stack->depth();
+
+  ZStack* label = new ZStack(GREY, width, height, depth, 1);
+  labelStack(label);
+
+  uint8_t *p = stack->array8();
+  uint8_t *pend = p + stack->getVoxelNumber();
+  uint8_t *q = stack->array8();
+
+  for(; p != pend; ++p, ++q)
+  {
+    *q = *q * *p;
+  }
+
+  delete label;
+}
+
+
+void ZSegmentationScan::addSegment(int z, int y, int sx, int ex)
+{
+  getStrip(z,y).push_back(sx);
+  getStrip(z,y).push_back(ex);
+}
+
+
 //implementation for ZSegmentationNode
 void ZSegmentationNode::updateChildrenLabel()
 {
--- neurolabi/gui/sandbox/zmultiscalesegmentationmanagement.h
+++ neurolabi/gui/sandbox/zmultiscalesegmentationmanagement.h
@@ -6,6 +6,7 @@
 #include <memory>
 #include <QWidget>
 #include <QTreeView>
+#include "zintpoint.h"
 #include "zsandboxmodule.h"
 
 class ZObject3dScan;
@@ -13,6 +14,51 @@ class ZObject3dScanArray;
 class QStandardItem;
 class ZStack;
 class ZStackObject;
+class ZIntCuboid;
+
+class ZSegmentationScan
+{
+public:
+ZSegmentationScan();
+~ZSegmentationScan();
+//void init();
+
+public:
+void loadStack(ZStack* stack);
+void maskStack(ZStack* stack);
+void labelStack(ZStack* stack, int v=1);
+
+//void setOffset(ZIntPoint offset){m_offset = offset;}
+//void setOffset(int x, int y, int z){m_offset = ZIntPoint(x,y,z);}
+//ZIntPoint getOffset()const{return m_offset;}
+void unify(ZSegmentationScan* obj);
+inline std::vector<int>& getStrip(int z, int y){return (z < m_sz || z > m_ez || y < m_sy || y > m_ey) ? m_empty_vec : getSlice(z)[y-m_sy];}
+inline std::vector<std::vector<int>>& getSlice(int z){return (z < m_sz || z > m_ez) ? m_empty_vec_vec : m_data[z-m_sz];}
+std::vector<std::vector<std::vector<int>>>& getData(){return m_data;}
+public:
+inline int minZ()const {return m_sz;}
+inline int maxZ()const {return m_ez;}
+inline int minY()const {return m_sy;}
+inline int maxY()const {return m_ey;}
+
+private:
+inline void addSegment(int z, int y, int sx, int ex);
+ZIntCuboid getStackForegroundBoundBox(ZStack* stack);
+//void canonizeStrip(int z, int y);
+std::vector<int> canonize(std::vector<int>&a, std::vector<int>& b);
+
+void clearData();
+void preprareData(int depth, int height);
+
+private:
+std::vector<std::vector<std::vector<int>>> m_data;
+std::vector<int> m_empty_vec;
+std::vector<std::vector<int>> m_empty_vec_vec;
+//ZIntPoint m_offset;
+int m_sz, m_ez;
+int m_sy, m_ey;
+};
+
 
 class ZSegmentationNode
 {
