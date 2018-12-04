dbd951bf6b94e977c55cff937fe2cc28e6316464
Ting Zhao <tingzhao@gmail.com>
2018-12-04 10:48:44 -0500

    more flexible configuration for warning about body status merging

--- neurolabi/gui/flyem/zflyembodyannotationmerger.h
+++ neurolabi/gui/flyem/zflyembodyannotationmerger.h
@@ -5,10 +5,12 @@
 #include <string>
 #include <vector>
 #include <set>
+#include <QMap>
 
 #include "zflyembodystatus.h"
 
 class ZJsonObject;
+class ZFlyEmBodyAnnotation;
 
 class ZFlyEmBodyAnnotationMerger
 {
@@ -22,17 +24,25 @@ public:
 
   void reset();
 
-  void print();
+  void print() const;
 
   const std::vector<ZFlyEmBodyStatus>& getStatusList() const;
 
+  const ZFlyEmBodyStatus &getBodyStatus(const std::string &name) const;
+
   int getStatusRank(const std::string &status) const;
+  bool isFinal(const std::string &status) const;
+//  bool hasConflict(const std::string &s1, const std::string &s2) const;
+
+  std::vector<std::vector<uint64_t>> getConflictBody(
+      const QMap<uint64_t, ZFlyEmBodyAnnotation> &annotMap) const;
 
 private:
   std::vector<ZFlyEmBodyStatus> m_statusList;
 
-  std::unordered_map<std::string, int> m_statusRank;
+  std::unordered_map<std::string, ZFlyEmBodyStatus> m_statusMap;
   std::vector<std::set<std::string>> m_conflictStatus;
+  ZFlyEmBodyStatus m_emptyStatus;
 };
 
 #endif // ZFLYEMBODYANNOTATIONMERGER_H
