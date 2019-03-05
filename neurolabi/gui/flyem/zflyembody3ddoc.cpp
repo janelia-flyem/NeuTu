cbc21193ba3089e307c7d2c791f7c27698fed240
Ting Zhao <tingzhao@gmail.com>
2019-03-05 16:11:53 -0500

    fix body upload time profiling in neu3 splitting

--- neurolabi/gui/flyem/zflyembody3ddoc.cpp
+++ neurolabi/gui/flyem/zflyembody3ddoc.cpp
@@ -4037,8 +4037,12 @@ void ZFlyEmBody3dDoc::commitSplitResult()
           newBodyId = m_mainDvidWriter.writeSplit(*seg, remainderId, 0);
           uploadingTime += timer.elapsed();
         } else {
+          QElapsedTimer timer;
+          timer.start();
           std::pair<uint64_t, uint64_t> idPair =
               m_mainDvidWriter.writeSupervoxelSplit(*seg, remainderId);
+          uploadingTime += timer.elapsed();
+
           remainderId = idPair.first;
           newBodyId = idPair.second;
 
